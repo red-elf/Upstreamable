174cf14f84b5ef29c21f8a7187fd2d26230a1ed9
Milos Vasic <i@mvasic.ru>
2022-09-21 16:23:11 +0300

    Installation script - Work in progress.

--- install_upstreams.sh
+++ install_upstreams.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
 
-echo "Tbd."
-exit 1
+DIR_UPSTREAMS="Upstreams"
+
+if [ -n "$1" ]; then
+
+  DIR_UPSTREAMS="$1"
+fi
+
+echo "Upstreams sources path: '$DIR_UPSTREAMS'"
+
+function UNSET_UPSTREAM_VARIABLES {
+
+  unset UPSTREAMABLE_REPOSITORY
+
+  if [ -n "$UPSTREAMABLE_REPOSITORY" ]; then
+
+    echo "ERROR: The UPSTREAMABLE_REPOSITORY environment variable is still set"
+    exit 1
+  fi
+}
+
+for i in "$DIR_UPSTREAMS"/*.sh; do
+
+  UNSET_UPSTREAM_VARIABLES
+
+  if test -e "$i"; then
+
+    echo "Processing upstream file: $i"
+
+    # shellcheck disable=SC1090
+    . "$i"
+
+    echo "Upstream: $UPSTREAMABLE_REPOSITORY"
+
+  else
+
+    echo "ERROR: '$i' not found at: '$(pwd)'"
+    exit 1
+  fi
+done
